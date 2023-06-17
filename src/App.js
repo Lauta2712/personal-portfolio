926960dce7b2e99fc5a1b5c20653a9ae15767cf1
Lauta2712 <lausjrc01@gmail.com>
2023-06-17 13:12:15 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Services from './pages/Services/Services';
+
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/contact' element={<Contact />}/>
-      <Route path='/services' element={<Services/>}/>
     </Routes>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import styles from "./Home.module.css";
 import Skills from "../../components/Skills/Skills";
 import Projects from "../../components/Projects/Projects";
-import Navbar from "../../components/Navbar/Navbar";
+// import Navbar from "../../components/Navbar/Navbar";
+import About from "../../components/About/About";
+import Contact from "../../components/Contact/Contact";
 
 const Home = () => {
     return (
         <div className={styles.home}>
-            <Navbar/>
+            <h1>Lautaro Rodriguez</h1>
+            <h2 className={styles.fullstack}>Full Stack Developer</h2>
             <Skills/>
             <Projects/>
+            <About/>
+            <Contact/>
         </div>
     )
 }
