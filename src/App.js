886a04e898bdc15c63a58148e08c346f1276ee57
Lauta2712 <lausjrc01@gmail.com>
2024-08-24 21:10:11 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 
-
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-    </Routes>
+    <Router basename="/personal-portfolio">
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
