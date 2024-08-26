d8516b1d48678f7c5725966c3718fc77df7a777c
Lauta2712 <lausjrc01@gmail.com>
2024-08-26 10:58:37 -0300

    New icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "./Footer.css";
-import LinkedIn from "../../icons/linkedin.png";
-// import Gmail from "../../icons/gmail.png";
-import GitHub from "../../icons/github (2).png";
-import Instagram from "../../icons/instagram.png";
-import { useTranslation } from 'react-i18next';  
+import { FaLinkedin, FaGithub, FaInstagram, FaWhatsapp } from "react-icons/fa"; 
 
 
 const Footer = () => {
-    const { t } = useTranslation();
-
     return (
         <footer id="footer" className="footer">
             <a href="https://www.linkedin.com/in/lautaro-rodriguez-a87ba1231" target="_blank" rel="noopener noreferrer">
-                <img src={LinkedIn} alt="LinkedIn" />
+                <FaLinkedin size={40} style={{ color: '#0077B5' }} />
             </a>
             <a href="https://github.com/Lauta2712" target="_blank" rel="noopener noreferrer">
-                <img src={GitHub} alt="GitHub" />
+                <FaGithub size={40} style={{ color: '#333' }} /> 
             </a>
             <a href="https://instagram.com/lauta_rodriguez01/" target="_blank" rel="noopener noreferrer">
-                <img src={Instagram} alt="Instagram" />
+                <FaInstagram size={40} style={{ color: '#E4405F' }} />
+            </a>
+            <a href="https://wa.me/2645189148" target="_blank" rel="noopener noreferrer">
+                <FaWhatsapp size={40} style={{ color: '#25D366' }} />
             </a>
         </footer>
     );
