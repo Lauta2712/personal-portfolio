10c47283aa85ca7a5cdb8936a38af9e1ace82893
Lauta2712 <lausjrc01@gmail.com>
2025-02-04 10:41:02 -0300

    Date detail

--- src/i18n/i18nConfig.js
+++ src/i18n/i18nConfig.js
@@ -55,7 +55,7 @@ i18n
           
           "jobTitle5": "Full Stack Developer",
           "company5": "Coneq",
-          "date5": "December 2024 - Present",
+          "date5": "Dec 2024 - Present",
           "responsibility12": "Developing and optimizing software solutions for the construction industry.",
           "responsibility13": "Collaborating with cross-functional teams to define project requirements and deadlines.",
           "responsibility14": "Leading the development of new features for our core product.",
@@ -157,7 +157,7 @@ i18n
           "databaseTech": "Base de datos: PostgreSQL, Sequelize.",
           "jobTitle5": "Full Stack Developer",
           "company5": "Coneq",
-          "date5": "Diciembre 2024 - Actualidad",
+          "date5": "Dic 2024 - Actualidad",
           "responsibility12": "Desarrollando y optimizando soluciones de software para la industria de la construcción.",
           "responsibility13": "Colaborando con equipos multifuncionales para definir requisitos y plazos de proyectos.",
           "responsibility14": "Liderando el desarrollo de nuevas funcionalidades para nuestro producto principal.",
