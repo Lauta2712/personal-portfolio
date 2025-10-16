455169161130a1e3c1fb5b1a04b2bb62548ca5d4
Lauta2712 <lausjrc01@gmail.com>
2025-10-16 10:34:06 -0300

    CV by selected lang

--- src/components/HeroContent/HeroContent.jsx
+++ src/components/HeroContent/HeroContent.jsx
@@ -3,26 +3,31 @@ import "./HeroContent.css";
 import { useTranslation } from 'react-i18next';
 
 const HeroContent = () => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+
+    const currentLang = i18n.language;    
+
+    const cvFile = currentLang === "es" || currentLang.startsWith("es")
+            ? "/docs/Lautaro_Rodriguez_CV.pdf"
+            : "/docs/Lautaro_Rodriguez_CV_EN.pdf";
 
     return (
         <main id="hero" className="hero-section">
             <div className="hero-content">
                 <h1>{t('greeting')} <span className="highlight">{t('name')}</span></h1>
                 <h2 className="hero-subtitle">{t('heroSubtitle')}</h2>
-                {/* <p>{t('profession')}</p> */}
                 
                 <div className="hero-buttons">
-                    <a href="/docs/Lautaro_Rodriguez_CV.pdf" className="pdf" download>
+                    <a href={cvFile} className="pdf" download>
                         <button>{t('download cv')}</button>
                     </a>
                 </div>
 
                 <div className="scroll-icon">
-                <svg width="30" height="30" fill="none" stroke="#00BFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
-                    <line x1="12" y1="5" x2="12" y2="19"/>
-                    <polyline points="19 12 12 19 5 12"/>
-                </svg>
+                    <svg width="30" height="30" fill="none" stroke="#00BFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                        <line x1="12" y1="5" x2="12" y2="19"/>
+                        <polyline points="19 12 12 19 5 12"/>
+                    </svg>
                 </div>
             </div>
         </main>
